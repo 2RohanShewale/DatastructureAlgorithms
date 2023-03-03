0f291febe0681b233d4072b40aaca52aa0db0569
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-03 16:53:28 +0530

    [Rohan]Finding word in array using binary search

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -11,24 +11,30 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Permutations permutations = new Permutations();
-
-            Console.Write("1.Permutation\nEnter your choice:");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
+            while (true)
             {
-                case 1:
-                    Console.Write("Enter a string: ");
-                    string str = Console.ReadLine();
-                    int n = str.Length;
-                    permutations.permute(str, 0, n - 1);
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice.");
-                    break;
+                Console.Write("1.Permutation\nEnter your choice:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Permutations permutations = new Permutations();
+                        Console.Write("Enter a string: ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        permutations.permute(str, 0, n - 1);
+                        break;
+                    case 2:
+                        BinarySearch binarySearch = new BinarySearch();
+                        Console.WriteLine("Enter word to search:");
+                        str = Console.ReadLine();
+                        binarySearch.search(str);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
             }
-            Console.ReadKey();
-
         }
     }
 }
