d0943512a6c59734eff7268312fe8d70e7294f1d
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-03 16:40:30 +0530

    [Rohan]Add: permutations of strings

---
+++ Algorithms/Algorithms/Permutations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class Permutations
+    {
+        public void permute(string str,
+                                int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+
+        public string swap(string a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutations permutations = new Permutations();
+
+            Console.Write("1.Permutation\nEnter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter a string: ");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    permutations.permute(str, 0, n - 1);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
